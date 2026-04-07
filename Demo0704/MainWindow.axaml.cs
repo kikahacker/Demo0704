dc8652e6cd8d40c6283f316c539e9cee278678a6
Class_Student <Class_Student@rtk.local>
2026-04-07 09:45:37 +0300

    1

---
+++ Demo0704/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Demo0704
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
